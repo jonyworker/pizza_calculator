ae71ba30d456bb7091f878e9bc7e8bef5df11b05
jony wu <37248488+jonyworker@users.noreply.github.com>
2025-10-17 18:15:26 +0800

    已完成變數宣告及計算邏輯

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,11 @@ import Calculator from './components/Calculator.vue'
   <h1 class="text-3xl font-bold underline bg-sky-500">
     Hello world!
   </h1>
-  <Calculator></Calculator>
+  <div class="bg-white dark:bg-gray-900 dark:scheme-dark">
+    <div class="mx-auto max-w-7xl px-4 py-24 sm:px-6 sm:py-32 lg:px-8">
+      <Calculator />
+    </div>
+  </div>
 </template>
 
 <style scoped>
